fc847ad04365d8dd08b5dcf8032867cc5901f299
bmm349 <bmm349@drexel>
2020-01-31 22:45:32 -0500

    initial inventory skeleton code

---
+++ Assets/InventorySystem/ItemDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class ItemDatabase : MonoBehaviour
+{
+    public static ItemDatabase _instance;
+    public List<Item> items;
+    // Start is called before the first frame update
+    public ItemDatabase getInstance()
+    {
+        return _instance;
+    }
+    public List<Item> getItems()
+    {
+        return _instance.items;
+    }
+}
