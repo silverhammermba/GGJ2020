1fb831aaaa25a2b527b3ae6861c9fccbb7a42b60
Max <silverhammermba@gmail.com>
2020-02-01 00:23:24 -0500

    pressing A tests if jump would work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpDistance;
+    public float jumpSpeed;
+    public float jumpHeight;
+
     Rigidbody2D rigidBody;
+    CircleCollider2D playerCollider;
+    CircleCollider2D jumpTester;
+    Vector2 move;
+    bool startedTestingJump;
+    bool jumpFailed;
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponents<CircleCollider2D>()[0];
+        jumpTester = GetComponents<CircleCollider2D>()[1];
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        jumpTester.radius = playerCollider.radius;
+        startedTestingJump = false;
+        jumpFailed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null) return;
+
+        move = gamepad.leftStick.ReadValue() * speed;
+
+        if (startedTestingJump)
+        {
+            startedTestingJump = false;
+            if (jumpFailed)
+            {
+                // TODO: indicate jump failed somehow????
+                Debug.Log("Jump failed");
+            }
+            else
+            {
+                // TODO: do jump
+                Debug.Log("Jump succeeded");
+            }
+        }
+        else if (gamepad.buttonSouth.isPressed)
+        {
+            StartJump(move);
+        }
+
+        rigidBody.AddForce(move);
+    }
+
+    void StartJump(Vector2 direction)
+    {
+        if (startedTestingJump) return;
+
+        startedTestingJump = true;
+        jumpFailed = false;
+        jumpTester.offset = move.normalized * jumpDistance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (startedTestingJump)
+        {
+            jumpFailed = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (startedTestingJump)
+        {
+            jumpFailed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
     }
 }
