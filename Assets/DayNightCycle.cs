1554c126b1224f8c5345d00e0411725400f45dac
Max <silverhammermba@gmail.com>
2020-02-01 23:33:15 -0500

    add day/night cycle

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public float dayDuration;
+    Animation anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim["DayNight"].wrapMode = WrapMode.Loop;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dayDuration != 0)
+        {
+            anim["DayNight"].speed = 1.0f / dayDuration;
+        }
+    }
+}
